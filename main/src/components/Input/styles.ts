0e875f0c6ff979a9f3e6a44475eb418e5e0988df
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2021-04-19 23:08:52 -0300

    Update login page and input

--- main/src/components/Input/styles.ts
+++ main/src/components/Input/styles.ts
@@ -1,5 +1,24 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+form{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+form label{
+  font-size: 1.4rem;
+}
+input{
+   background: #FFF;
+    width: 300px;
+    height: 50px;
+    padding: 0 0.5rem;
+    margin-top: 1rem;
+    outline: none;
+    color:#000;
+    font-size: 1rem;
+    border-radius: 8px;
+}
 
 `;
