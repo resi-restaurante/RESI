5158336057dd417f01b517c0a58a37a5df4b9c58
Yan Marinho <yankaiquecosta.yk@gmail.com>
2021-05-04 07:58:39 -0300

    feature: novo input

--- main/src/components/Input/index.tsx
+++ main/src/components/Input/index.tsx
@@ -1,15 +1,73 @@
-import { Container } from './styles';
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
+import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
+import { useField } from '@unform/core';
+import { Container, Error } from './styles';
 
-interface InputProps {
-  title: string;
-  descriptionPlaceholder: string;
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  containerStyle?: object;
+  icon?: React.ComponentType<IconBaseProps>;
 }
 
-export function Input({ title, descriptionPlaceholder }: InputProps) {
+const Input: React.FC<InputProps> = ({
+  name,
+  containerStyle = {},
+  icon: Icon,
+  ...rest
+}) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
   return (
-    <Container>
-      <p>{title}</p>
-      <input type="text" placeholder={descriptionPlaceholder} />
+    <Container
+      style={containerStyle}
+      isErrored={!!error}
+      isFilled={isFilled}
+      isFocused={isFocused}
+    >
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+      {Icon && <Icon size={20} />}
+      {error && (
+        <Error title={error}>
+          <FiAlertCircle color="#c53030" size={20} />
+        </Error>
+      )}
     </Container>
   );
-}
+};
+
+export default Input;
--- main/src/components/Input/styles.ts
+++ main/src/components/Input/styles.ts
@@ -1,25 +1,72 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import Tooltip from '../tooltip/index';
 
-export const Container = styled.div`
-  form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  background: #fff;
+  border-radius: 10px;
+  padding: 16px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  color: #6c6c6c;
+  border: 2px solid #fff;
+  & + div {
+    margin-top: 8px;
   }
-
-  form label {
-    font-size: 1.4rem;
-  }
-
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff6865;
+      border-color: #ff6865;
+    `}
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff6865;
+    `}
   input {
-    background: #fff;
-    width: 300px;
-    height: 50px;
-    padding: 0 0.5rem;
-    margin-top: 1rem;
-    outline: none;
     color: #000;
-    font-size: 1rem;
-    border-radius: 8px;
+    flex: 1;
+    background: transparent;
+    border: 0;
+    border-color: #fff;
+    height: 20px;
+    &::placeholder {
+      color: ff000ff;
+    }
+    &:focus {
+      box-shadow: 0 0 0 0;
+      border: 0 none;
+      outline: 0;
+    }
+  }
+  svg {
+    margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+  svg {
+    margin: 0;
+  }
+  span {
+    background: #c53030;
+    color: #fff;
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
