f51ad7b9601bb4b6b25d3c6b1633006f831c58eb
Yan Marinho <yankaiquecosta.yk@gmail.com>
2021-06-19 21:29:45 -0300

    style: correção de lint

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,15 +1,11 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes } from './pages/routes';
-import AppProvider from './core/hooks/index';
-
 import './assets/styles/animation.css';
 
 export function App() {
   return (
-    <AppProvider>
-      <Router>
-        <Routes />
-      </Router>
-    </AppProvider>
+    <Router>
+      <Routes />
+    </Router>
   );
 }
