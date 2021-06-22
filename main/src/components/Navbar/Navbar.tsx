6af083059af4740f6472394b2bd69113647fefc4
Yan Marinho <yankaiquecosta.yk@gmail.com>
2021-06-22 08:14:33 -0300

    feature: adicionando hook ao menu

--- main/src/components/Navbar/Navbar.tsx
+++ main/src/components/Navbar/Navbar.tsx
@@ -1,15 +1,20 @@
+import { FiUser } from 'react-icons/fi';
 import {
   ButtonLogin,
   Container,
   ContainerLabel,
   NavigationButton,
+  UserInformationContainer,
 } from './styles';
+
+import { useAuth } from '../../hooks/AuthContext';
 
 interface NavbarProps {
   itemVisible: boolean;
 }
 
 export const Navbar = ({ itemVisible }: NavbarProps) => {
+  const { user } = useAuth();
   return (
     <Container>
       <div>
@@ -18,10 +23,17 @@ export const Navbar = ({ itemVisible }: NavbarProps) => {
         </NavigationButton>
       </div>
       <ContainerLabel>
-        {itemVisible && (
+        {itemVisible && !user && (
           <NavigationButton to="/login">
             <ButtonLogin>Entrar</ButtonLogin>
           </NavigationButton>
+        )}
+        {user && (
+          <UserInformationContainer to="/profile">
+            <FiUser />
+            <h4>Olá, </h4>
+            <h4 id="userName">{user.nome}</h4>
+          </UserInformationContainer>
         )}
       </ContainerLabel>
     </Container>
