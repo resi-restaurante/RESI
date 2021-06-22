81ba78bfd4a36c4e26c16779f2f2ee3e5f0ecc15
Yan Marinho <yankaiquecosta.yk@gmail.com>
2021-06-22 08:13:53 -0300

    feature: atualizando tipagem dos hooks

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes } from './routes';
+import { AuthProvider } from './hooks/AuthContext';
 import './assets/styles/animation.css';
 
 export function App() {
   return (
-    <Router>
-      <Routes />
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes />
+      </Router>
+    </AuthProvider>
   );
 }
