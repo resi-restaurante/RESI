4c66b183cecdba70fc7c03aebaa8ebc727d31055
Yan Marinho <yankaiquecosta.yk@gmail.com>
2021-06-23 08:28:12 -0300

    feature: adicionanando listagem de dados

--- main/src/pages/Profile/index.tsx
+++ main/src/pages/Profile/index.tsx
@@ -1,15 +1,120 @@
-import { Container, CardUserInformationContainer } from './styles';
+import { useState } from 'react';
+import { FiHome, FiCalendar, FiSearch } from 'react-icons/fi';
+import {
+  Container,
+  RedContainer,
+  CardUserInformationContainer,
+  SelectionMenu,
+  ContentContainer,
+  ListContainer,
+  ScrollView,
+} from './styles';
+import {
+  Header,
+  CardListAppointment,
+  CardListCompanys,
+  InputWithOutForm,
+  Footer,
+} from '../../components';
 import profileAvatar from '../../assets/images/profile.jpg';
+import { useAuth } from '../../hooks/AuthContext';
 
 export default function ProfilePage() {
+  const { user } = useAuth();
+
+  const [showSchedule, setShowSchedule] = useState(true);
+  const [showCompanys, setShowCompanys] = useState(false);
+
+  const handleDisableContainer = (containerType: string) => {
+    if (containerType === 'schedule') {
+      setShowSchedule(true);
+      setShowCompanys(false);
+    } else {
+      setShowSchedule(false);
+      setShowCompanys(true);
+    }
+  };
+
   return (
     <Container>
-      <CardUserInformationContainer>
-        <div>
-          <img src={profileAvatar} alt="Foto do usuário" />
-        </div>
-        <h3>Yan Kaique Costa Marinho</h3>
-      </CardUserInformationContainer>
+      <Header />
+      <RedContainer>
+        <CardUserInformationContainer>
+          <div>
+            <img src={profileAvatar} alt="Foto do usuário" />
+          </div>
+          <div>
+            <h3>
+              {user.nome}#{user.id}
+            </h3>
+            <h3 id="user-email">{user.email}</h3>
+          </div>
+        </CardUserInformationContainer>
+        <SelectionMenu>
+          <ContentContainer
+            onPressContent={showSchedule}
+            onClick={() => handleDisableContainer('schedule')}
+          >
+            <FiCalendar size={20} />
+            <h3>Agendamentos</h3>
+          </ContentContainer>
+          <ContentContainer
+            onPressContent={showCompanys}
+            onClick={() => handleDisableContainer('companys')}
+          >
+            <FiHome size={20} />
+            <h3>Companhias</h3>
+          </ContentContainer>
+        </SelectionMenu>
+      </RedContainer>
+      {showSchedule && (
+        <ListContainer>
+          <header>
+            <h1>Pesquise aqui por seus agendamentos</h1>
+            <InputWithOutForm icon={FiSearch} />
+          </header>
+          <ScrollView>
+            <CardListAppointment
+              id="1A2VQ"
+              restaurantName="Casa da Eshiha"
+              date="01/07/2022"
+            />
+            <CardListAppointment
+              id="1A2VQ"
+              restaurantName="Casa da Eshiha"
+              date="01/07/2022"
+            />
+            <CardListAppointment
+              id="1A2VQ"
+              restaurantName="Casa da Eshiha"
+              date="01/07/2022"
+            />
+            <CardListAppointment
+              id="1A2VQ"
+              restaurantName="Casa da Eshiha"
+              date="01/07/2022"
+            />
+          </ScrollView>
+        </ListContainer>
+      )}
+      {showCompanys && (
+        <ListContainer>
+          <header>
+            <h1>Pesquise aqui por sua companhia</h1>
+            <InputWithOutForm icon={FiSearch} />
+          </header>
+          <ScrollView>
+            <CardListCompanys id="1A2VQ" restaurantName="Casa da Eshiha" />
+            <CardListCompanys id="1A2VQ" restaurantName="Casa da Eshiha" />
+            <CardListCompanys id="1A2VQ" restaurantName="Casa da Eshiha" />
+            <CardListCompanys id="1A2VQ" restaurantName="Casa da Eshiha" />
+            <CardListCompanys id="1A2VQ" restaurantName="Casa da Eshiha" />
+            <CardListCompanys id="1A2VQ" restaurantName="Casa da Eshiha" />
+            <CardListCompanys id="1A2VQ" restaurantName="Casa da Eshiha" />
+          </ScrollView>
+        </ListContainer>
+      )}
+      <Footer />
     </Container>
   );
 }
--- main/src/pages/Profile/styles.ts
+++ main/src/pages/Profile/styles.ts
@@ -1,16 +1,33 @@
 import styled from 'styled-components';
 
+interface ConteinerBottomBorder {
+  onPressContent: boolean;
+}
+
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const RedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
   background-color: #e53935;
-  height: 100%;
+  border-bottom: #f3f3f3;
+  width: 100vw;
 `;
 
 export const CardUserInformationContainer = styled.header`
   display: flex;
-  width: 80%;
-
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  margin-top: 1rem;
+  text-align: center;
   img {
     width: 100px;
     height: 100px;
@@ -19,5 +36,83 @@ export const CardUserInformationContainer = styled.header`
 
   h3 {
     margin: 1rem 0 0 1rem;
+  }
+
+  #user-email {
+    font-style: italic;
+    font-weight: 300;
+    font-size: 16px;
+  }
+`;
+
+export const SelectionMenu = styled.div`
+  display: flex;
+  align-self: center;
+  background-color: transparent;
+  cursor: pointer;
+  @media screen and (max-width: 720px) {
+    flex-direction: column;
+  }
+  div {
+    margin: 4rem;
+    h3 {
+      font-size: 20px;
+    }
+  }
+`;
+
+export const ContentContainer = styled.div<ConteinerBottomBorder>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-bottom: ${props => (props.onPressContent ? '3px solid #fff' : '')};
+`;
+
+export const ListContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 4rem;
+
+  header {
+    width: 100%;
+    flex-direction: column;
+    display: flex;
+    margin-bottom: 3rem;
+    align-items: center;
+
+    h1 {
+      margin-bottom: 0.5rem;
+      color: #2b2b2b;
+    }
+  }
+`;
+
+export const ScrollView = styled.div`
+  align-self: center;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  height: 20rem;
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 3px grey;
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #e53935;
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background: #e53935;
   }
 `;
