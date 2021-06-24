bd3a292dea964ebc93e6d463a4a3b4b171f036e8
Yan Marinho <yan.contato.dev@gmail.com>
2021-06-24 15:12:53 -0300

    feature: tela de detalhes do restaurante

--- main/src/pages/DetailRestaurant/index.tsx
+++ main/src/pages/DetailRestaurant/index.tsx
@@ -1,7 +1,16 @@
 import { useEffect, useState } from 'react';
-import DetailsRestaurantWithBorder from '../../components/DetailsRestaurantWithBorder';
-import { Footer, Header } from '../../components';
-import { Container } from './styles';
+import { FiMail, FiPhoneCall, FiExternalLink } from 'react-icons/fi';
+import { AiOutlineWhatsApp } from 'react-icons/ai';
+import { Footer, Header, Button } from '../../components';
+import {
+  Container,
+  ContentRestaurant,
+  Title,
+  Adress,
+  Description,
+  RestaurantGrid,
+  InformationContainer,
+} from './styles';
 import RestaurantData from '../../dtos/Restaurant';
 
 function DetailRestaurant(props: any) {
@@ -14,12 +23,44 @@ function DetailRestaurant(props: any) {
   return (
     <Container>
       <Header />
+      <RestaurantGrid>
+        <ContentRestaurant>
+          <img src="images/restaurante.jpg" alt="" />
 
-      <DetailsRestaurantWithBorder
-        name={restaurantInfo.nome}
-        description={restaurantInfo.descricao}
-      />
-
+          <div>
+            <Title>{restaurantInfo.nome}</Title>
+            <Adress>
+              <p>
+                {restaurantInfo.endereco}, {restaurantInfo.cidade} -{' '}
+                {restaurantInfo.estado}
+              </p>
+              <a
+                href="https://www.google.com/maps/search/restaurant+augusto+aparecida+sp"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <FiExternalLink size={15} />
+              </a>
+            </Adress>
+            <Description>{restaurantInfo.descricao}</Description>
+          </div>
+        </ContentRestaurant>
+        <InformationContainer>
+          <div>
+            <FiPhoneCall size={25} />
+            <p>{restaurantInfo.telefone}</p>
+          </div>
+          <div>
+            <FiMail size={25} />
+            <p>{restaurantInfo.email}</p>
+          </div>
+          <div>
+            <AiOutlineWhatsApp size={25} />
+            <p>{restaurantInfo.telefone}</p>
+          </div>
+        </InformationContainer>
+        <Button>Reservar Agora !</Button>
+      </RestaurantGrid>
       <Footer />
     </Container>
   );
--- main/src/pages/DetailRestaurant/styles.ts
+++ main/src/pages/DetailRestaurant/styles.ts
@@ -1,7 +1,111 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  footer {
-    margin-top: 30rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RestaurantGrid = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media screen and (max-width: 720px) {
+    width: 100%;
+  }
+  background: #ffffff;
+  border-radius: 2rem;
+  box-shadow: 20px 20px 50px -30px #e53935;
+  margin-top: 1rem;
+  img {
+    @media screen and (max-width: 720px) {
+      width: 15rem;
+      height: 10rem;
+      margin: 0 auto;
+    }
+    width: 20rem;
+    height: 14rem;
+    margin: 3.7rem;
+    border-radius: 1rem;
+  }
+
+  button {
+    align-self: center;
+    margin: 1rem;
+    width: 90%;
+  }
+`;
+
+export const ContentRestaurant = styled.div`
+  @media screen and (max-width: 720px) {
+    margin: 1rem;
+  }
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+
+  div {
+    margin-top: 4rem;
+  }
+`;
+
+export const Title = styled.h1`
+  color: #000;
+`;
+
+export const Adress = styled.h4`
+  display: flex;
+  color: #e53935;
+  p {
+    font-style: italic;
+    font-size: 16px;
+    font-weight: 300;
+  }
+
+  svg {
+    cursor: pointer;
+    margin-left: 0.5rem;
+  }
+`;
+
+export const Description = styled.p`
+  margin-top: 1rem;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 200;
+  font-size: 16px;
+  line-height: 16px;
+  width: 90%;
+  color: #000000;
+`;
+
+export const InformationContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-self: center;
+
+  align-items: center;
+  width: 60%;
+
+  margin-bottom: 2rem;
+  cursor: pointer;
+  color: #b11f2b;
+
+  flex-wrap: wrap;
+
+  @media screen and (max-width: 720px) {
+    justify-content: center;
+  }
+
+  p {
+    margin-top: 1rem;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 1rem 1rem;
   }
 `;
